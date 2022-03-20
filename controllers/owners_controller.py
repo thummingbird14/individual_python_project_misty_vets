ae5b313b1dbc5ff6ee0a6cc9fd523121a35eca7b
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-03-20 13:33:29 +0000

    remaining functions for owners

--- controllers/owners_controller.py
+++ controllers/owners_controller.py
@@ -26,3 +26,26 @@ def create_owner():
 def show_owner(id):
     owner = owner_repository.select(id)
     return render_template('owners/show.html', owner = owner)
+
+@owners_blueprint.route("/owners/<id>/edit", methods=['GET'])
+def edit_owner(id):
+    owner = owner_repository.select(id)
+    return render_template('owners/edit.html', owner=owner)
+
+@owners_blueprint.route("/owners/<id>", methods=['POST'])
+def update_owner(id):
+    name = request.form['name']
+    address = request.form['address']
+    phone_no = request.form['phone_no']
+    owner = Owner(name, address, phone_no, id)
+    owner_repository.update(owner)
+    return redirect('/owners')
+
+@owners_blueprint.route("/owners/<id>/delete")
+def delete_owner(id):
+    owner_repository.delete(id)
+    return redirect('/owners')
+
+
+
+    
